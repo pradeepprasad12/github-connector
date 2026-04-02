44aee7fbf86f7567f8ce11f6f911faa8819c6f03
pradeep prasad <prasad.pradeep.pp@gmail.com>
2026-04-02 13:39:16 +0530

    Updated API improvements and bug fixes

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -12,6 +12,6 @@ class PullRequest(BaseModel):
     owner: str = Field(..., min_length=1)
     repo: str = Field(..., min_length=1)
     title: str = Field(..., min_length=2)
-    body: str = Field(..., min_length=1)
+    body: str = Field(..., min_length=2)
     head: str = Field(..., min_length=1)
     base: str = Field(..., min_length=1)
